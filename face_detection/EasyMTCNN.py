dc94774fcbe10c9bdfaf2d5d980e03e698be18f9
Josh Varty <joshvarty@gmail.com>
2020-02-17 15:00:32 -0500

    MTCNN wrapper

---
+++ face_detection/EasyMTCNN.py
@@ -0,0 +1,32 @@
+import cv2
+import PIL
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pathlib import Path
+from facenet_pytorch import MTCNN
+from utils import read_frame
+
+
+class EasyMTCNN:
+
+    def __init__(self):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.detector = MTCNN(device=device, post_process=False)
+
+    def detect(self, frame):
+        img = PIL.Image.fromarray(frame)
+        face = self.detector(img).permute(1, 2, 0) / 255
+        return face
+
+
+if __name__ == '__main__':
+    # Test code
+    video_path = '../data/dfdc_train_part_2/abmjszfycr_REAL.mp4'
+    frame = read_frame(video_path)
+
+    easyMTCNN = EasyMTCNN()
+    face = easyMTCNN.detect(frame)
+    plt.imshow(face)
+    plt.show()
