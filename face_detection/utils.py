5958445779161d87cbec7dbb072f97d43e637236
Josh Varty <joshvarty@gmail.com>
2020-02-21 15:59:05 -0500

    Allow for reading random frames

--- face_detection/utils.py
+++ face_detection/utils.py
@@ -1,3 +1,4 @@
+import random
 import cv2
 
 def read_frame_as_size(video_path, size=(128, 128)):
@@ -31,3 +32,32 @@ def read_all_frames(video_path):
 
     capture.release()
     return all_frames
+
+
+def read_random_frame(video_path):
+    capture = cv2.VideoCapture(str(video_path))
+    frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    random_frame = int(random.random() * frame_count)
+    # Set to read specific frame
+    capture.set(cv2.CAP_PROP_POS_FRAMES, random_frame)
+    ret, frame = capture.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    capture.release()
+    return frame
+
+
+def read_random_sequential_frames(video_path, num_frames=4):
+
+    capture = cv2.VideoCapture(str(video_path))
+    frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT)) - num_frames
+    random_frame = int(random.random() * frame_count)
+    capture.set(cv2.CAP_PROP_POS_FRAMES, random_frame)
+    frames = []
+    for i in range(num_frames):
+        # Set to read specific frame
+        ret, frame = capture.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frames.append(frame)
+
+    capture.release()
+    return frames
