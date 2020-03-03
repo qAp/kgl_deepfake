fa4bfd6014ab7eef4243d8f1f3b2c3aeed1babd1
Josh Varty <joshvarty@gmail.com>
2020-03-02 21:42:23 -0500

    Add plot_detections

--- face_detection/utils.py
+++ face_detection/utils.py
@@ -1,5 +1,10 @@
-import random
 import cv2
+import torch
+import random
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 def read_frame_as_size(video_path, size=(128, 128)):
     capture = cv2.VideoCapture(str(video_path))
@@ -40,9 +45,21 @@ def read_random_frame(video_path):
     """
     capture = cv2.VideoCapture(str(video_path))
     frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
-    random_frame = int(random.random() * frame_count)
+    # HACK: Some videos are missing the last 10 frames. No idea why.
+    random_frame = int(random.random() * frame_count) - 10
+    print(random_frame)
     # Set to read specific frame
     capture.set(cv2.CAP_PROP_POS_FRAMES, random_frame)
+    ret, frame = capture.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    capture.release()
+    return frame
+
+
+def read_frame_at_frame_number(video_path, frame_number):
+    capture = cv2.VideoCapture(str(video_path))
+    # Set to read specific frame
+    capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
     ret, frame = capture.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     capture.release()
@@ -68,3 +85,26 @@ def read_random_sequential_frames(video_path, num_frames=4):
 
     capture.release()
     return np.array(frames)
+
+
+def plot_detections(img, detections, with_keypoints=True, figsize=(10, 10)):
+    fig, ax = plt.subplots(1, figsize=figsize)
+    ax.grid(False)
+    ax.imshow(img)
+
+    if isinstance(detections, torch.Tensor):
+        detections = detections.cpu().numpy()
+
+    print("Found %d faces" % len(detections))
+
+    for i in range(len(detections)):
+        xmin = detections[i, 0]
+        ymin = detections[i, 1]
+        xmax = detections[i, 2]
+        ymax = detections[i, 3]
+
+        rect = patches.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                 linewidth=1, edgecolor="r", facecolor="none")
+        ax.add_patch(rect)
+
+    plt.show()
